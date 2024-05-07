1afb4557a98f9ed10aa96ba76d3dea039e3d619d
Lucaszaranha <lucaszaranha@gmail.com>
2024-05-07 12:25:20 -0300

    Implementação da camada controller

--- app/src/main/java/devandroid/lucas/applistavip/controller/PessoaController.java
+++ app/src/main/java/devandroid/lucas/applistavip/controller/PessoaController.java
@@ -1,4 +1,13 @@
 package devandroid.lucas.applistavip.controller;
 
+import androidx.annotation.NonNull;
+
 public class PessoaController {
+
+
+    @NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
