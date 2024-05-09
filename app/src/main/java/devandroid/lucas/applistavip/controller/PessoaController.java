ccd08df614703c19b7418b3932ac260b8e4f68ab
Lucaszaranha <lucaszaranha@gmail.com>
2024-05-09 16:29:33 -0300

    Refaturação

--- app/src/main/java/devandroid/lucas/applistavip/controller/PessoaController.java
+++ app/src/main/java/devandroid/lucas/applistavip/controller/PessoaController.java
@@ -1,11 +1,55 @@
 package devandroid.lucas.applistavip.controller;
 
 
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import devandroid.lucas.applistavip.model.Pessoa;
+import devandroid.lucas.applistavip.view.MainActivity;
 
 public class PessoaController {
 
+    SharedPreferences preferences;
+    SharedPreferences.Editor listaVip;
+
+    public static final String NOME_PREFERENCES = "pref_listvip";
+
+    public PessoaController(MainActivity mainActivity) {
+        preferences = mainActivity.getSharedPreferences(NOME_PREFERENCES, 0);
+
+        listaVip = preferences.edit();
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 
     public void salvar(Pessoa pessoa) {
+        Log.i("MVC_Controller", "Salvo: " + pessoa.toString());
+
+        listaVip.putString("primeiroNome", pessoa.getPrimeiroNome());
+        listaVip.putString("sobreNome", pessoa.getSobreNome());
+        listaVip.putString("cursoDesejado", pessoa.getCursoDesejado());
+        listaVip.putString("telefoneContato", pessoa.getTelefoneContato());
+        listaVip.apply();
+    }
+
+    public Pessoa buscar(Pessoa pessoa) {
+
+        pessoa.setPrimeiroNome(preferences.getString("primeiroNome", ""));
+        pessoa.setSobreNome(preferences.getString("sobreNome", ""));
+        pessoa.setCursoDesejado(preferences.getString("cursoDesejado", ""));
+        pessoa.setTelefoneContato(preferences.getString("telefoneContato", ""));
+        return pessoa;
+
+    }
+    public void limpar() {
+        listaVip.clear();
+        listaVip.apply();
+
     }
 }
